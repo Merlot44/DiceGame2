615d456abc8d08a036f5adb1f7edb5a472888487
Merlot44 <stevegaret448@gmail.com>
2025-02-28 23:56:42 -0500

    Finished the basic logic for the first screen.

--- Form1.vb
+++ Form1.vb
@@ -1,2 +1,33 @@
 ﻿Public Class WNDMain
+    ' Define a variable for the player's name
+    Public PlayerName As String
+    Private Sub BTNStart_Click(sender As Object, e As EventArgs) Handles BTNStart.Click
+
+        ' Verify that the user entered their name
+        If TBXName.Text <> "" Then
+            ' Save the player's name
+            PlayerName = TBXName.Text
+
+            ' Hide used elements
+            LBLName.Visible = False
+            TBXName.Visible = False
+            LBLNameHint.Visible = False
+            BTNStart.Visible = False
+
+            ' Welcome player
+            LBLMain.Text = "Bienvenue " + PlayerName + "!"
+
+            ' Show new elements
+            BTNInstructions.Visible = True
+            BTNContinue.Visible = True
+        Else
+            ' Show the player name hint
+            LBLNameHint.Visible = True
+        End If
+    End Sub
+
+    Private Sub TBXName_TextChanged(sender As Object, e As EventArgs) Handles TBXName.TextChanged
+        ' Hide the player name hint
+        LBLNameHint.Visible = False
+    End Sub
 End Class
