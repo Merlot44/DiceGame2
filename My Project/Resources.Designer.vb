c2db560a96e0508efdec4caa78ca069acde39d9e
Merlot44 <stevegaret448@gmail.com>
2025-03-03 22:44:04 -0500

    Added a sound effect ressource.

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -71,6 +71,16 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized resource of type System.Byte[].
+        '''</summary>
+        Friend ReadOnly Property Click() As Byte()
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Click", resourceCulture)
+                Return CType(obj,Byte())
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
         Friend ReadOnly Property Dice1() As System.Drawing.Bitmap
