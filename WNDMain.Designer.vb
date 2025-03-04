c1afb6bd6c32ca1b11d5986fbe10a72040d66d92
Merlot44 <stevegaret448@gmail.com>
2025-03-03 22:41:33 -0500

    Add project files.

---
+++ WNDMain.Designer.vb
@@ -0,0 +1,322 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class WNDMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WNDMain))
+        PBXSound = New PictureBox()
+        TBXGuess = New TextBox()
+        BTNRetry = New Button()
+        LBLWonGames = New Label()
+        LBLPlayedGames = New Label()
+        BTNGuess = New Button()
+        LBLGuessHint = New Label()
+        LBLGuess = New Label()
+        BTNContinue = New Button()
+        BTNInstructions = New Button()
+        BTNStart = New Button()
+        LBLNameHint = New Label()
+        TBXName = New TextBox()
+        LBLName = New Label()
+        PBXDice1 = New PictureBox()
+        PBXDice2 = New PictureBox()
+        TBXInstructions = New TextBox()
+        LBLMain = New Label()
+        LBLWinPercentage = New Label()
+        CType(PBXSound, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PBXDice1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PBXDice2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PBXSound
+        ' 
+        PBXSound.BackColor = Color.Transparent
+        PBXSound.Image = My.Resources.Resources.Unmute
+        PBXSound.Location = New Point(2, 5)
+        PBXSound.Name = "PBXSound"
+        PBXSound.Size = New Size(32, 32)
+        PBXSound.SizeMode = PictureBoxSizeMode.StretchImage
+        PBXSound.TabIndex = 41
+        PBXSound.TabStop = False
+        ' 
+        ' TBXGuess
+        ' 
+        TBXGuess.Location = New Point(463, 244)
+        TBXGuess.Name = "TBXGuess"
+        TBXGuess.Size = New Size(87, 23)
+        TBXGuess.TabIndex = 40
+        TBXGuess.Visible = False
+        ' 
+        ' BTNRetry
+        ' 
+        BTNRetry.Font = New Font("Comic Sans MS", 30F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        BTNRetry.Location = New Point(346, 270)
+        BTNRetry.Name = "BTNRetry"
+        BTNRetry.Size = New Size(313, 81)
+        BTNRetry.TabIndex = 37
+        BTNRetry.Text = "Réessayer!"
+        BTNRetry.UseVisualStyleBackColor = True
+        BTNRetry.Visible = False
+        ' 
+        ' LBLWonGames
+        ' 
+        LBLWonGames.BackColor = Color.Transparent
+        LBLWonGames.Font = New Font("Comic Sans MS", 15F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LBLWonGames.ForeColor = Color.White
+        LBLWonGames.Location = New Point(372, 504)
+        LBLWonGames.Name = "LBLWonGames"
+        LBLWonGames.Size = New Size(268, 28)
+        LBLWonGames.TabIndex = 35
+        LBLWonGames.Text = "Parties gagnées : 0"
+        LBLWonGames.TextAlign = ContentAlignment.MiddleCenter
+        LBLWonGames.Visible = False
+        ' 
+        ' LBLPlayedGames
+        ' 
+        LBLPlayedGames.BackColor = Color.Transparent
+        LBLPlayedGames.Font = New Font("Comic Sans MS", 15F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LBLPlayedGames.ForeColor = Color.White
+        LBLPlayedGames.Location = New Point(2, 504)
+        LBLPlayedGames.Name = "LBLPlayedGames"
+        LBLPlayedGames.Size = New Size(344, 28)
+        LBLPlayedGames.TabIndex = 34
+        LBLPlayedGames.Text = "Parties jouées : 0"
+        LBLPlayedGames.TextAlign = ContentAlignment.MiddleLeft
+        LBLPlayedGames.Visible = False
+        ' 
+        ' BTNGuess
+        ' 
+        BTNGuess.Font = New Font("Comic Sans MS", 30F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        BTNGuess.Location = New Point(372, 397)
+        BTNGuess.Name = "BTNGuess"
+        BTNGuess.Size = New Size(268, 81)
+        BTNGuess.TabIndex = 33
+        BTNGuess.Text = "Deviner!"
+        BTNGuess.UseVisualStyleBackColor = True
+        BTNGuess.Visible = False
+        ' 
+        ' LBLGuessHint
+        ' 
+        LBLGuessHint.AutoSize = True
+        LBLGuessHint.BackColor = Color.Transparent
+        LBLGuessHint.Font = New Font("Comic Sans MS", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LBLGuessHint.ForeColor = Color.White
+        LBLGuessHint.Location = New Point(404, 270)
+        LBLGuessHint.Name = "LBLGuessHint"
+        LBLGuessHint.Size = New Size(208, 17)
+        LBLGuessHint.TabIndex = 32
+        LBLGuessHint.Text = "Veuillez entrer une valeur de 2 à 12!"
+        LBLGuessHint.Visible = False
+        ' 
+        ' LBLGuess
+        ' 
+        LBLGuess.AutoSize = True
+        LBLGuess.BackColor = Color.Transparent
+        LBLGuess.Font = New Font("Comic Sans MS", 20F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LBLGuess.ForeColor = Color.White
+        LBLGuess.Location = New Point(447, 203)
+        LBLGuess.Name = "LBLGuess"
+        LBLGuess.Size = New Size(123, 38)
+        LBLGuess.TabIndex = 31
+        LBLGuess.Text = "Somme :"
+        LBLGuess.Visible = False
+        ' 
+        ' BTNContinue
+        ' 
+        BTNContinue.Font = New Font("Comic Sans MS", 25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        BTNContinue.Location = New Point(652, 230)
+        BTNContinue.Name = "BTNContinue"
+        BTNContinue.Size = New Size(245, 100)
+        BTNContinue.TabIndex = 29
+        BTNContinue.Text = "Continuer!"
+        BTNContinue.UseVisualStyleBackColor = True
+        BTNContinue.Visible = False
+        ' 
+        ' BTNInstructions
+        ' 
+        BTNInstructions.Font = New Font("Comic Sans MS", 25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        BTNInstructions.Location = New Point(101, 230)
+        BTNInstructions.Name = "BTNInstructions"
+        BTNInstructions.Size = New Size(245, 100)
+        BTNInstructions.TabIndex = 28
+        BTNInstructions.Text = "Instructions"
+        BTNInstructions.UseVisualStyleBackColor = True
+        BTNInstructions.Visible = False
+        ' 
+        ' BTNStart
+        ' 
+        BTNStart.Font = New Font("Comic Sans MS", 30F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        BTNStart.Location = New Point(372, 397)
+        BTNStart.Name = "BTNStart"
+        BTNStart.Size = New Size(268, 81)
+        BTNStart.TabIndex = 27
+        BTNStart.Text = "Commencer!"
+        BTNStart.UseVisualStyleBackColor = True
+        ' 
+        ' LBLNameHint
+        ' 
+        LBLNameHint.AutoSize = True
+        LBLNameHint.BackColor = Color.Transparent
+        LBLNameHint.Font = New Font("Comic Sans MS", 10F)
+        LBLNameHint.ForeColor = Color.Beige
+        LBLNameHint.Location = New Point(422, 270)
+        LBLNameHint.Name = "LBLNameHint"
+        LBLNameHint.Size = New Size(180, 19)
+        LBLNameHint.TabIndex = 26
+        LBLNameHint.Text = "Veuilles entrer votre nom!"
+        LBLNameHint.Visible = False
+        ' 
+        ' TBXName
+        ' 
+        TBXName.Location = New Point(346, 244)
+        TBXName.Name = "TBXName"
+        TBXName.Size = New Size(347, 23)
+        TBXName.TabIndex = 25
+        ' 
+        ' LBLName
+        ' 
+        LBLName.AutoSize = True
+        LBLName.BackColor = Color.Transparent
+        LBLName.Font = New Font("Comic Sans MS", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LBLName.ForeColor = Color.Beige
+        LBLName.Location = New Point(346, 189)
+        LBLName.Name = "LBLName"
+        LBLName.Size = New Size(347, 38)
+        LBLName.TabIndex = 24
+        LBLName.Text = "Veuillez entrer vôtre nom:"
+        ' 
+        ' PBXDice1
+        ' 
+        PBXDice1.Location = New Point(32, 179)
+        PBXDice1.Name = "PBXDice1"
+        PBXDice1.Size = New Size(256, 256)
+        PBXDice1.SizeMode = PictureBoxSizeMode.StretchImage
+        PBXDice1.TabIndex = 38
+        PBXDice1.TabStop = False
+        PBXDice1.Visible = False
+        ' 
+        ' PBXDice2
+        ' 
+        PBXDice2.Location = New Point(719, 179)
+        PBXDice2.Name = "PBXDice2"
+        PBXDice2.Size = New Size(256, 256)
+        PBXDice2.SizeMode = PictureBoxSizeMode.StretchImage
+        PBXDice2.TabIndex = 39
+        PBXDice2.TabStop = False
+        PBXDice2.Visible = False
+        ' 
+        ' TBXInstructions
+        ' 
+        TBXInstructions.Font = New Font("Comic Sans MS", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TBXInstructions.Location = New Point(101, 179)
+        TBXInstructions.Multiline = True
+        TBXInstructions.Name = "TBXInstructions"
+        TBXInstructions.ReadOnly = True
+        TBXInstructions.Size = New Size(239, 195)
+        TBXInstructions.TabIndex = 30
+        TBXInstructions.Text = resources.GetString("TBXInstructions.Text")
+        TBXInstructions.Visible = False
+        ' 
+        ' LBLMain
+        ' 
+        LBLMain.BackColor = Color.Transparent
+        LBLMain.Font = New Font("Comic Sans MS", 40F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LBLMain.ForeColor = Color.Beige
+        LBLMain.Location = New Point(2, 23)
+        LBLMain.Name = "LBLMain"
+        LBLMain.Size = New Size(1005, 190)
+        LBLMain.TabIndex = 23
+        LBLMain.Text = "Bienvenue au jeu de simulation de dés!"
+        LBLMain.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' LBLWinPercentage
+        ' 
+        LBLWinPercentage.BackColor = Color.Transparent
+        LBLWinPercentage.Font = New Font("Comic Sans MS", 15F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LBLWinPercentage.ForeColor = Color.White
+        LBLWinPercentage.Location = New Point(652, 504)
+        LBLWinPercentage.Name = "LBLWinPercentage"
+        LBLWinPercentage.Size = New Size(355, 28)
+        LBLWinPercentage.TabIndex = 36
+        LBLWinPercentage.Text = "Pourcentage de victoire : 0%"
+        LBLWinPercentage.TextAlign = ContentAlignment.MiddleRight
+        LBLWinPercentage.Visible = False
+        ' 
+        ' WNDMain
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.Background
+        BackgroundImageLayout = ImageLayout.Center
+        ClientSize = New Size(1008, 537)
+        Controls.Add(PBXSound)
+        Controls.Add(TBXGuess)
+        Controls.Add(BTNRetry)
+        Controls.Add(LBLWonGames)
+        Controls.Add(LBLPlayedGames)
+        Controls.Add(BTNGuess)
+        Controls.Add(LBLGuessHint)
+        Controls.Add(LBLGuess)
+        Controls.Add(BTNContinue)
+        Controls.Add(BTNInstructions)
+        Controls.Add(BTNStart)
+        Controls.Add(LBLNameHint)
+        Controls.Add(TBXName)
+        Controls.Add(LBLName)
+        Controls.Add(PBXDice1)
+        Controls.Add(PBXDice2)
+        Controls.Add(TBXInstructions)
+        Controls.Add(LBLMain)
+        Controls.Add(LBLWinPercentage)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "WNDMain"
+        Text = "Dice Game 2"
+        CType(PBXSound, ComponentModel.ISupportInitialize).EndInit()
+        CType(PBXDice1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PBXDice2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PBXSound As PictureBox
+    Friend WithEvents TBXGuess As TextBox
+    Friend WithEvents BTNRetry As Button
+    Friend WithEvents LBLWonGames As Label
+    Friend WithEvents LBLPlayedGames As Label
+    Friend WithEvents BTNGuess As Button
+    Friend WithEvents LBLGuessHint As Label
+    Friend WithEvents LBLGuess As Label
+    Friend WithEvents BTNContinue As Button
+    Friend WithEvents BTNInstructions As Button
+    Friend WithEvents BTNStart As Button
+    Friend WithEvents LBLNameHint As Label
+    Friend WithEvents TBXName As TextBox
+    Friend WithEvents LBLName As Label
+    Friend WithEvents PBXDice1 As PictureBox
+    Friend WithEvents PBXDice2 As PictureBox
+    Friend WithEvents TBXInstructions As TextBox
+    Friend WithEvents LBLMain As Label
+    Friend WithEvents LBLWinPercentage As Label
+
+End Class
---
+++ WNDMain.vb
@@ -0,0 +1,244 @@
+﻿Imports System.Security.Authentication.ExtendedProtection
+
+Public Class WNDMain
+    ' Define a variable for the player's name
+    Public PlayerName As String = "joueur"
+
+    ' Define a variable for the guessed sum of the die
+    Public DiceGuess As Integer = 12
+
+    ' Define variables fot the die roll
+    Public Dice1Roll As Integer = 6
+    Public Dice2Roll As Integer = 6
+
+    ' Define a variable for sound control
+    Public Sound As Boolean = True
+
+    ' Define variables for statistics
+    Public PlayedGames As Integer = 0
+    Public WonGames As Integer = 0
+    Public WinPercentage As Integer = 0
+    Private Sub BTNStart_Click(sender As Object, e As EventArgs) Handles BTNStart.Click
+        ' Verify sound status
+        If Sound = True Then
+            ' Play the click sound
+            ' My.Computer.Audio.Play(My.Resources.Click, AudioPlayMode.Background)
+        End If
+
+        ' Verify that the user entered their name
+        If TBXName.Text <> "" Then
+            ' Save the player's name
+            PlayerName = TBXName.Text
+
+            ' Hide used elements
+            LBLName.Visible = False
+            TBXName.Visible = False
+            LBLNameHint.Visible = False
+            BTNStart.Visible = False
+
+            ' Welcome player
+            LBLMain.Text = "Bienvenue " + PlayerName + "!"
+
+            ' Show new elements
+            BTNInstructions.Visible = True
+            BTNContinue.Visible = True
+        Else
+            ' Show the player name hint
+            LBLNameHint.Visible = True
+        End If
+    End Sub
+
+    Private Sub TBXName_TextChanged(sender As Object, e As EventArgs) Handles TBXName.TextChanged
+        ' Hide the player name hint
+        LBLNameHint.Visible = False
+    End Sub
+
+    Private Sub BTNInstructions_Click(sender As Object, e As EventArgs) Handles BTNInstructions.Click
+        ' Verify sound status
+        If Sound = True Then
+            ' Play the click sound
+            ' My.Computer.Audio.Play(My.Resources.Click, AudioPlayMode.Background)
+        End If
+
+        ' Hide the button
+        BTNInstructions.Visible = False
+
+        ' Show instructions
+        TBXInstructions.Visible = True
+    End Sub
+
+    Private Sub BTNContinue_Click(sender As Object, e As EventArgs) Handles BTNContinue.Click
+        ' Verify sound status
+        If Sound = True Then
+            ' Play the click sound
+            ' My.Computer.Audio.Play(My.Resources.Click, AudioPlayMode.Background)
+        End If
+
+        ' Hide used elements
+        BTNInstructions.Visible = False
+        BTNContinue.Visible = False
+        TBXInstructions.Visible = False
+
+        ' Prompt the player to guess the dice
+        LBLMain.Text = "Veuillez deviner la somme des dés :"
+
+        ' Show next elements
+        LBLGuess.Visible = True
+        TBXGuess.Visible = True
+        BTNGuess.Visible = True
+        LBLPlayedGames.Visible = True
+        LBLWonGames.Visible = True
+        LBLWinPercentage.Visible = True
+    End Sub
+
+    Private Sub BTNGuess_Click(sender As Object, e As EventArgs) Handles BTNGuess.Click
+        ' Verify that the player's input is valid
+        If TBXGuess.Text = "2" Or TBXGuess.Text = "3" Or TBXGuess.Text = "4" Or TBXGuess.Text = "5" Or TBXGuess.Text = "6" Or TBXGuess.Text = "7" Or TBXGuess.Text = "8" Or TBXGuess.Text = "9" Or TBXGuess.Text = "10" Or TBXGuess.Text = "11" Or TBXGuess.Text = "12" Then
+            ' Save the guessed value
+            DiceGuess = CInt(TBXGuess.Text)
+
+            ' Save random values from 1 to 6 for the dice roll
+            Dice1Roll = Rnd() * 6 + 1
+            Dice2Roll = Rnd() * 6 + 1
+
+            ' Hide used elements
+            LBLGuess.Visible = False
+            TBXGuess.Visible = False
+            LBLGuessHint.Visible = False
+            BTNGuess.Visible = False
+
+            ' Compare the player's guess to the die roll
+            If DiceGuess = Dice1Roll + Dice2Roll Then
+                ' Announce the player's win
+                LBLMain.Text = "Félicitations " + PlayerName + ", vôtre choix (" + CStr(DiceGuess) + ") est correcte!"
+
+                ' Increment the number of won games
+                WonGames += 1
+
+                ' Play the win sound
+                ' My.Computer.Audio.Play(My.Resources.Win, AudioPlayMode.Background)
+            Else
+                ' Announce the player's loss
+                LBLMain.Text = "Désolé " + PlayerName + ", vôtre choix (" + CStr(DiceGuess) + ") est incorrecte!"
+
+                ' Play the loss sound
+                ' My.Computer.Audio.Play(My.Resources.Loss, AudioPlayMode.Background)
+            End If
+
+            ' Increment the number of played games
+            PlayedGames += 1
+
+            ' Calculate win percentage
+            WinPercentage = WonGames / PlayedGames * 100
+
+            ' Change the statistics
+            LBLPlayedGames.Text = "Parties jouées : " + CStr(PlayedGames)
+            LBLWonGames.Text = "Parties gagnées : " + CStr(WonGames)
+            LBLWinPercentage.Text = "Pourcentage de victoire : " + CStr(WinPercentage) + "%"
+
+            ' Show the correct image to represent the first dice in the die roll
+            If Dice1Roll = 1 Then
+                PBXDice1.Image = My.Resources.Dice1
+            ElseIf Dice1Roll = 2 Then
+                PBXDice1.Image = My.Resources.Dice2
+            ElseIf Dice1Roll = 3 Then
+                PBXDice1.Image = My.Resources.Dice3
+            ElseIf Dice1Roll = 4 Then
+                PBXDice1.Image = My.Resources.Dice4
+            ElseIf Dice1Roll = 5 Then
+                PBXDice1.Image = My.Resources.Dice5
+            ElseIf Dice1Roll = 6 Then
+                PBXDice1.Image = My.Resources.Dice6
+            End If
+
+            ' Show the correct image to represent the second dice in the die roll
+            If Dice2Roll = 1 Then
+                PBXDice2.Image = My.Resources.Dice1
+            ElseIf Dice2Roll = 2 Then
+                PBXDice2.Image = My.Resources.Dice2
+            ElseIf Dice2Roll = 3 Then
+                PBXDice2.Image = My.Resources.Dice3
+            ElseIf Dice2Roll = 4 Then
+                PBXDice2.Image = My.Resources.Dice4
+            ElseIf Dice2Roll = 5 Then
+                PBXDice2.Image = My.Resources.Dice5
+            ElseIf Dice2Roll = 6 Then
+                PBXDice2.Image = My.Resources.Dice6
+            End If
+
+            ' Show next elements
+            PBXDice1.Visible = True
+            PBXDice2.Visible = True
+            BTNRetry.Visible = True
+        Else
+            ' Show the dice guess hint
+            LBLGuessHint.Visible = True
+
+            ' Verify sound status
+            If Sound = True Then
+                ' Play the click sound
+                ' My.Computer.Audio.Play(My.Resources.Click, AudioPlayMode.Background)
+            End If
+        End If
+    End Sub
+
+    Private Sub TBXDice_TextChanged(sender As Object, e As EventArgs) Handles TBXGuess.TextChanged
+        LBLGuessHint.Visible = False
+    End Sub
+
+    Private Sub BTNRetry_Click(sender As Object, e As EventArgs) Handles BTNRetry.Click
+        ' Verify sound status
+        If Sound = True Then
+            ' Play the click sound
+            ' My.Computer.Audio.Play(My.Resources.Click, AudioPlayMode.Background)
+        End If
+
+        ' Hide used elements
+        PBXDice1.Visible = False
+        PBXDice2.Visible = False
+        BTNRetry.Visible = False
+
+        ' Reset main text
+        LBLMain.Text = "Veuillez deviner la somme des dés :"
+
+        ' Reset guessing text box
+        TBXGuess.Text = ""
+
+        ' Show next elements
+        LBLGuess.Visible = True
+        TBXGuess.Visible = True
+        BTNGuess.Visible = True
+
+        ' Reset single-use variables
+        DiceGuess = 12
+        Dice1Roll = 6
+        Dice2Roll = 6
+    End Sub
+
+    Private Sub PBXSound_Click(sender As Object, e As EventArgs) Handles PBXSound.Click
+        ' Change the sound status
+        Sound = Not Sound
+
+        If Sound = True Then
+            ' Play the click sound
+            ' My.Computer.Audio.Play(My.Resources.Click, AudioPlayMode.Background)
+
+            ' Set the image to the green speaker
+            PBXSound.Image = My.Resources.Unmute
+
+            'Play the background music
+            ' My.Computer.Audio.Play(My.Resources.Music, AudioPlayMode.BackgroundLoop)
+        Else
+            ' Stop all audio
+            My.Computer.Audio.Stop()
+
+            ' Set the image to the red speaker
+            PBXSound.Image = My.Resources.Mute
+        End If
+    End Sub
+
+    Private Sub WNDMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' Play the background music
+        ' My.Computer.Audio.Play(My.Resources.Music, AudioPlayMode.BackgroundLoop)
+    End Sub
+End Class
